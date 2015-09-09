2cd0bb0142bf8c65765483623e8d4c9df09f8e51
Ryan Gee <ryan.cv.gee@gmail.com>
2015-09-09 02:58:17 -0400

    Remove unnecessary store dependency.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 const React = require('react/addons');
 const Editor = require('./components/Editor');
-const CurrentDirectoryStore = require('./stores/CurrentDirectoryStore');
 
 module.exports = () => {
   React.render(<Editor />, document.body);

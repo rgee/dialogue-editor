2bff2f476c42bbae043c9e7332c230451d8d125f
Ryan Gee <ryan.cv.gee@gmail.com>
2015-09-09 00:35:42 -0400

    Add gulp and some npm tasks to compile from es6

---
+++ tasks/build.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean');
+
+var buildDir = 'build';
+
+gulp.task('clean', function () {
+  return gulp.src(buildDir, { read: false })
+    .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function() {
+  return gulp.src('src/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(buildDir));
+});
